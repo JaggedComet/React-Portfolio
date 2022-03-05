0a4ebb631ca871f2446f103184001b18cef26dd7
Marco Flores <marcoflores1029@gmail.com>
2022-03-05 09:46:05 -0800

    added form

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,12 +1,88 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { validateEmail, checkmessage } from '../../utils/helpers';
+
 
 export default function Contact() {
+  // Create state variables for the fields in the form
+  // We are also setting their initial values to an empty string
+  const [email, setEmail] = useState('');
+  const [name, setname] = useState('');
+  const [message, setmessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleInputChange = (e) => {
+    // Getting the value and name of the input which triggered the change
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+    // Based on the input type, we set the state of either email, name, and message
+    if (inputType === 'email') {
+      setEmail(inputValue);
+    } else if (inputType === 'name') {
+      setname(inputValue);
+    } else {
+      setmessage(inputValue);
+    }
+  };
+
+  const handleFormSubmit = (e) => {
+    // Preventing the default behavior of the form submit (which is to refresh the page)
+    e.preventDefault();
+
+    // First we check to see if the email is not valid or if the name is empty. If so we set an error message to be displayed on the page.
+    if (!validateEmail(email) || !name) {
+      setErrorMessage('Email or name is invalid');
+      // We want to exit out of this code block if something is wrong so that the user can correct it
+      return;
+      // Then we check to see if the message is not valid. If so, we set an error message regarding the message.
+    }
+    if (!checkmessage(message)) {
+      setErrorMessage(
+        `Choose a more secure message for the account: ${name}`
+      );
+      return;
+    }
+    alert(`Hello ${name}`);
+
+    // If everything goes according to plan, we want to clear out the input after a successful registration.
+    setname('');
+    setmessage('');
+    setEmail('');
+  };
+
   return (
     <div>
-      <h1>Contact Page</h1>
-      <p>
-  
-      </p>
+      <p>Hello {name}</p>
+      <form className="form">
+        <input
+          value={name}
+          name="name"
+          onChange={handleInputChange}
+          type="text"
+          placeholder="name"
+        />
+        <input
+          value={email}
+          name="email"
+          onChange={handleInputChange}
+          type="email"
+          placeholder="email"
+        />
+        <input
+          value={message}
+          name="message"
+          onChange={handleInputChange}
+          type="message"
+          placeholder="message"
+        />
+        <button type="button" onClick={handleFormSubmit}>Submit</button>
+      </form>
+      {errorMessage && (
+        <div>
+          <p className="error-text">{errorMessage}</p>
+        </div>
+      )}
     </div>
   );
 }
